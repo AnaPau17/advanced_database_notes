4215ec41d805b7cb3617ccc7b7da2473a3b35ec2
AnaPau17 <anapaulanavh@gmail.com>
2026-02-17 08:30:01 -0600

    session-2026-02-17

---
+++ sql_challenges/challenge-02/solution.sql
@@ -0,0 +1,32 @@
+--Exercise 6
+SELECT movies.id, movies.title, boxoffice.domestic_sales, boxoffice.international_sales FROM movies 
+INNER JOIN boxoffice 
+    ON movies.id = boxoffice.movie_id;
+
+
+SELECT movies.id, movies.title, boxoffice.domestic_sales, boxoffice.international_sales FROM movies 
+INNER JOIN boxoffice 
+    ON movies.id = boxoffice.movie_id
+WHERE boxoffice.domestic_sales < boxoffice.international_sales;
+
+
+SELECT movies.id, movies.title, boxoffice.rating FROM movies 
+INNER JOIN boxoffice 
+    ON movies.id = boxoffice.movie_id
+    ORDER BY rating DESC;
+
+
+--Exercise 7
+SELECT * FROM buildings 
+INNER JOIN employees 
+    ON buildings.building_name = employees.building
+GROUP BY buildings.building_name;
+
+
+SELECT * FROM buildings;
+
+
+SELECT buildings.building_name, employees.role FROM buildings 
+LEFT JOIN employees 
+    ON buildings.building_name = employees.building
+GROUP BY employees.role, buildings.building_name;
