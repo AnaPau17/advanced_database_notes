0a97f92db62dd30002ea3b06930ea5d9d778efb9
AnaPau17 <anapaulanavh@gmail.com>
2026-02-24 08:24:28 -0600

    session_2026-02-24

---
+++ sql_challenges/challenge-03/solution.sql
@@ -0,0 +1,23 @@
+--Exercise 10
+SELECT role, MAX(years_employed) AS longest_time FROM employees;
+
+SELECT role, AVG(years_employed) AS average_years FROM employees
+GROUP BY role;
+
+SELECT building, SUM(years_employed) AS years_worked FROM employees
+GROUP BY building;
+
+
+--Exercise 11
+SELECT role, COUNT(*) AS num_artist FROM employees
+WHERE role = "Artist";
+
+SELECT role, COUNT(*) AS employees_role FROM employees
+GROUP BY role;
+
+SELECT role, SUM(years_employed) FROM employees
+WHERE role = "Engineer";
+--or
+SELECT role, SUM(years_employed) FROM employees
+GROUP BY role
+HAVING role = "Engineer";
